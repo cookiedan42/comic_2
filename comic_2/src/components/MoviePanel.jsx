d8a3d072a46f9449738b8a06996a22307d32a9f4
cookiedan42 <cookiedan42@gmail.com>
2023-10-22 19:39:33 +0800

    randomize movie text placement

--- comic_2/src/components/MoviePanel.jsx
+++ comic_2/src/components/MoviePanel.jsx
@@ -4,21 +4,40 @@ import { useState } from 'react';
 export default function MoviePanel({data}) {
 
     const [state, setState] = useState(0);
+    const [tb, setTB] = useState(true);
     const size = data.length-1;
 
     useEffect(
         () => {
             setTimeout(
-                () => setState(Math.min(state + 1, size)),
+                () => {setTB(Math.random()>0.5);setState(Math.min(state + 1, size))},
                 data[state].timeout
             )
         },
         [state,data,size]
     )
+
+
+    const top = ()=>{
+        if (state===0 || tb){
+            return (<div>{data[state].text}</div>);
+        }
+        else {
+            return (<div style={{color:'white'}}>_</div>);
+        }
+    }
+    const bot = ()=>{
+        if (state===0 || tb){
+            return (<div style={{color:'white'}}>_</div>);
+        } else{
+            return (<div>{data[state].text}</div>);
+        }
+    }
     return (
         < div class="areaDiv" style={{ flexDirection: "row" }} >
-            <div>{data[state].text}</div>
+            {top()}
             <img src={data[state].image} alt="" />
+            {bot()}
         </div >
     )
 
